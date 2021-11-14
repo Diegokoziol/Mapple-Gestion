666e2f1f9f4cc52c4aaad1267446df3ca57f6a2b
Pedro <pedrogabe@gmail.com>
2021-11-14 20:39:39 -0300

    Create ProductoDto.h

---
+++ Mapple Gestion/DTOs/ProductoDto.h
@@ -0,0 +1,13 @@
+#ifndef PRODUCTODTO_H_INCLUDED
+#define PRODUCTODTO_H_INCLUDED
+
+class ProductoDto
+{
+public:
+    int _codigoProducto;
+    char _descripcionProducto[30];
+    float _precioProducto;
+    int _stockDisponible;
+};
+
+#endif // PRODUCTODTO_H_INCLUDED
