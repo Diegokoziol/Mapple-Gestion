8cc63f4b4e8f12fa86c6882551f7c31e8106c77d
Pedro <pedrogabe@gmail.com>
2021-11-14 23:21:48 -0300

    Create ProductoRepositorio.h

---
+++ Mapple Gestion/Repositorios/ProductoRepositorio.h
@@ -0,0 +1,14 @@
+#ifndef PRODUCTOREPOSITORIO_H_INCLUDED
+#define PRODUCTOREPOSITORIO_H_INCLUDED
+
+#include "../DTOs/ProductoDto.h"
+
+class ProductoRepositorio
+{
+public:
+    static int agregar(ProductoDto producto);
+    static bool leer(int pos, ProductoDto &producto);
+    static bool sobreescribir(int pos, ProductoDto producto);
+};
+
+#endif // PRODUCTOREPOSITORIO_H_INCLUDED
