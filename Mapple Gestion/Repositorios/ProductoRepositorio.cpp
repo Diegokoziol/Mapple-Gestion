e703afe58a9e2a8e7c0adb266852647f7230a74c
Pedro <pedrogabe@gmail.com>
2021-11-16 00:13:02 -0300

    Se define ProductoRepositorio::sobreescribir

--- Mapple Gestion/Repositorios/ProductoRepositorio.cpp
+++ Mapple Gestion/Repositorios/ProductoRepositorio.cpp
@@ -22,3 +22,14 @@ bool ProductoRepositorio::leer(int pos, ProductoDto &producto)
     fclose(file);
     return read;
 }
+
+bool ProductoRepositorio::sobreescribir(int pos, ProductoDto producto)
+{
+    FILE *file = fopen("Productos.dat", "rb+");
+    if(file==NULL) return false;
+
+    fseek(file, sizeof(ProductoDto)*pos, SEEK_SET);
+    bool wrote = fwrite(&producto, sizeof(ProductoDto), 1, file);
+
+    return wrote;
+}
