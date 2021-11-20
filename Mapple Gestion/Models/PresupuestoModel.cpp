ddb3e8adf6c2f58f961566890a19d6fdfbdf0c21
Pedro <pedrogabe@gmail.com>
2021-11-20 14:59:38 -0300

    Modificación PresupuestoModel::agregarItem
    Agregar cantidad si ya existe un item con el mismo código de producto

--- Mapple Gestion/Models/PresupuestoModel.cpp
+++ Mapple Gestion/Models/PresupuestoModel.cpp
@@ -16,19 +16,31 @@ void PresupuestoModel::setFecha(Fecha fecha){
 void PresupuestoModel::setPlazo(int plazo){
     _plazo = plazo;
 }
+
 void PresupuestoModel::agregarItem(ItemPresupuestoModel itemPresupuesto){
+    size_t pos;
+    for(pos=0; pos<_items.size(); pos++)
+    {
+        if(itemPresupuesto.getProducto().getCodigoProducto() == _items[pos].getProducto().getCodigoProducto())
+        {
+            _items[pos].agregarCantidad(itemPresupuesto.getCantidad());
+            return;
+        }
+    }
+
     if(_items.size()>=20) return;
 
     int id=0;
-    for(size_t i = 0; i < _items.size(); i++){
-        if(_items[i].getId() > id){
-            id=_items[i].getId();
-        }
+    for(pos=0; pos<_items.size(); pos++)
+    {
+        if(_items[pos].getId() > id)
+            id = _items[pos].getId();
     }
     id++;
     itemPresupuesto.setId(id);
     _items.push_back(itemPresupuesto);
 }
+
 void PresupuestoModel::quitarItem(int codItem){
     for(size_t i = 0; i < _items.size(); i++){
         if(_items[i].getProducto().getCodigoProducto() == codItem){
@@ -36,18 +48,22 @@ void PresupuestoModel::quitarItem(int codItem){
         }
     }
 }
+
 int PresupuestoModel::getId(){
     return _id;
 }
+
 ItemPresupuestoModel PresupuestoModel::getItem(std::size_t pos){
     if(pos>_items.size())
         return ItemPresupuestoModel();
     else
         return _items[pos];
 }
+
 Fecha PresupuestoModel::getFecha(){
     return _fecha;
 }
+
 float PresupuestoModel::getMontoTotal(){
     float montoTotal = 0;
     for(std::size_t i = 0; i < _items.size(); i++){
@@ -55,12 +71,14 @@ float PresupuestoModel::getMontoTotal(){
     }
     return montoTotal;
 }
+
 bool PresupuestoModel::recalcular(){
     for(size_t i = 0; i < _items.size(); i++){
         _items[i].actualizarMontoUnitario();
     }
     return true;
 }
+
 bool PresupuestoModel::vencido(){
     return (_fecha+_plazo) <= Fecha();
 }
